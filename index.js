8e33649db828afcf3fe12b25f82d139261645087
Yemny <y.feliz1296@gmail.com>
2023-09-18 17:17:32 -0400

    created classes for shapes, square, circule, triangle. New svg file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const shapes = require('./lib/shapes')
 const circle = require('./lib/circle');
 const triangle = require('./lib/triangle');
 const square = require('./lib/square');
@@ -26,7 +27,12 @@ const questions = [
 
     {
         type: 'list',
-        name: 'shapes',
+        name: 'shape',
+        choices: [
+            'circle',
+            'square',
+            'triangle',
+        ],
         message: 'What shape do you want your logo to be?',
     },
 
@@ -36,4 +42,37 @@ const questions = [
         message: 'Please enter a color, a key word or hexadecimal number, for the text color',
     },
 
-]
+];
+
+
+
+const svgGenerator = ({ text, fontColor, shape, logoColor}) => {
+    let logoMaker;
+    switch (shape){
+        case "Circle":
+          logoMaker = new circle();
+          break;
+        case "Triangle":
+          logoMaker = new triangle();
+          break;
+        case "Square":
+          logoMaker = new square();
+          break;
+      }
+};
+
+
+const init = () => {
+    inquirer
+      .prompt(questions)
+      // Destructure the data for text, fontColor, shape, and color
+      .then(({ text, fontColor, shape, color }) => {
+        svgGenerator({ text, fontColor, shape, color });
+      })
+      .catch((err) => {
+        console.error("Error: ", err);
+      });
+  };
+  
+  init();
+
